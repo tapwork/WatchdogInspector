4cbac853476d29cdab92a2bba47d8d4525a74571
Christian Menschel <christian.menschel@xing.com>
2016-02-09 08:52:25 +0100

    Some clean ups

--- Classes/TWWatchdogInspector.m
+++ Classes/TWWatchdogInspector.m
@@ -11,47 +11,19 @@
 #import <execinfo.h>
 #import <YourStatusBar/TWYourStatusBar.h>
 
-static CFRunLoopObserverRef kObserverRef;
-static double kBestWatchdog = 60.0;
+static const double kBestWatchdogFramerate = 60.0;
 static UILabel *kTextLabel = nil;
 static int kNumberOfFrames = 0;
-static CFTimeInterval kUpdateWatchdogInterval = 2.0;
-static CFTimeInterval kWatchdogTreshholdTimeInterval = 0.2;
+static const CFTimeInterval kUpdateWatchdogInterval = 2.0;
+static CFTimeInterval kWatchdogMaximumStallingTimeInterval = 0.5;
 static dispatch_source_t kWatchdogTimer;
+static CFRunLoopTimerRef kMainthreadTimer;
 
 @implementation TWWatchdogInspector
 
-static void timerWatchdogCallback(CFRunLoopTimerRef timer, void *info)
+static void mainthreadTimerCallback(CFRunLoopTimerRef timer, void *info)
 {
     kNumberOfFrames++;
-}
-
-static void runloopObserverCallback(CFRunLoopObserverRef observer, CFRunLoopActivity activity, void *info)
-{
-    if (activity == kCFRunLoopAfterWaiting) {
-        NSLog(@"====================");
-        NSLog(@"entry");
-    } else if (activity == kCFRunLoopBeforeWaiting) {
-        NSLog(@"exit");
-        NSLog(@"====================");
-    }
-}
-
-static void updateColorWithWatchdog(double Watchdog)
-{
-    //fade from green to red
-    double n = 1 - (Watchdog/60);
-    double red = (255 * n);
-    double green = (255 * (1 - n)/2);
-    double blue = 0;
-    UIColor *color = [UIColor colorWithRed:red/255.0 green:green/255.0 blue:blue/255.0 alpha:1.0];
-    if (Watchdog > 0.0) {
-        kTextLabel.backgroundColor = color;
-        kTextLabel.text = [NSString stringWithFormat:@"fps: %.2f", Watchdog];
-    } else {
-        kTextLabel.backgroundColor = [UIColor lightGrayColor];
-        kTextLabel.text = nil;
-    }
 }
 
 #pragma mark - Start / Stop
@@ -62,95 +34,85 @@ static void updateColorWithWatchdog(double Watchdog)
  
     [self addWatchdogTimer];
     [self addMainThreadWatchdogCounter];
- //   [self addRunLoopObserver];
 
     if (!kTextLabel) {
         [self setupStatusBarLabel];
     }
 }
 
++ (void)stop
+{
+    if (kWatchdogTimer) {
+        dispatch_source_cancel(kWatchdogTimer);
+        kWatchdogTimer = nil;
+    }
+
+    if (kMainthreadTimer) {
+        CFRunLoopTimerInvalidate(kMainthreadTimer);
+        kMainthreadTimer = nil;
+    }
+}
+
+#pragma mark - Private methods
+
 + (void)addMainThreadWatchdogCounter
 {
     CFRunLoopRef runLoop = CFRunLoopGetMain();
     CFRunLoopTimerContext timerContext = {0, NULL, NULL, NULL, NULL};
-    CFTimeInterval updateWatchdog = 1/kBestWatchdog;
+    CFTimeInterval updateWatchdog = 1/kBestWatchdogFramerate;
     CFRunLoopTimerRef timer = CFRunLoopTimerCreate(kCFAllocatorDefault,
                                                    0,
                                                    updateWatchdog,
                                                    0,
                                                    0,
-                                                   &timerWatchdogCallback,
+                                                   &mainthreadTimerCallback,
                                                    &timerContext);
 
     CFRunLoopAddTimer(runLoop, timer, kCFRunLoopCommonModes);
-}
-
-+ (void)addRunLoopObserver
-{
-    CFRunLoopObserverContext context = {0, (__bridge void *)(self),
-        NULL,
-        NULL,
-        NULL
-    };
-    
-    kObserverRef = CFRunLoopObserverCreate(kCFAllocatorDefault,
-                                           kCFRunLoopAllActivities,
-                                           YES,
-                                           0,
-                                           &runloopObserverCallback,
-                                           &context);
-    CFRunLoopAddObserver(CFRunLoopGetMain(), kObserverRef, kCFRunLoopCommonModes);
+    kMainthreadTimer = timer;
 }
 
 + (void)addWatchdogTimer
 {
-    kWatchdogTimer = createDispatchTimer(kUpdateWatchdogInterval * NSEC_PER_SEC,
-                                         (kUpdateWatchdogInterval * NSEC_PER_SEC) / 10,
-                                         dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-                                             
-                                             double fps = kNumberOfFrames/kUpdateWatchdogInterval;
-                                             kNumberOfFrames = 0;
-                                             NSLog(@"fps %.2f", fps);
-                                             CFTimeInterval startTime = CACurrentMediaTime();
-                                             dispatch_async(dispatch_get_main_queue(), ^{
-                                                 updateColorWithWatchdog(fps);
-                                                 CFTimeInterval endTime = CACurrentMediaTime();
-                                                 if (endTime - startTime > kWatchdogTreshholdTimeInterval) {
-                                                     NSLog(@"Mainthread stalled for %.2f seconds", endTime - startTime);
-                                                 }
-                                             });
-                                         });
+    kWatchdogTimer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0));
+    if (kWatchdogTimer) {
+        dispatch_source_set_timer(kWatchdogTimer, dispatch_walltime(NULL, 0), kUpdateWatchdogInterval * NSEC_PER_SEC, (kUpdateWatchdogInterval * NSEC_PER_SEC) / 10);
+        dispatch_source_set_event_handler(kWatchdogTimer, ^{
+            double fps = kNumberOfFrames/kUpdateWatchdogInterval;
+            kNumberOfFrames = 0;
+            NSLog(@"fps %.2f", fps);
+            
+            CFTimeInterval startTime = CACurrentMediaTime();
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self updateColorWithFPS:fps];
+                CFTimeInterval endTime = CACurrentMediaTime();
+                if (endTime - startTime > kWatchdogMaximumStallingTimeInterval) {
+                    NSLog(@"Mainthread stalled for %.2f seconds", endTime - startTime);
+                    
+                }
+            });
+        });
+        dispatch_resume(kWatchdogTimer);
+    }
 }
 
-dispatch_source_t createDispatchTimer(uint64_t interval, uint64_t leeway, dispatch_queue_t queue, dispatch_block_t block)
-{
-    dispatch_source_t timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
-    if (timer) {
-        dispatch_source_set_timer(timer, dispatch_walltime(NULL, 0), interval, leeway);
-        dispatch_source_set_event_handler(timer, block);
-        dispatch_resume(timer);
-    }
-    return timer;
-}
+#pragma mark - UI Updates
 
-+ (void)stop
++ (void)updateColorWithFPS:(double)fps
 {
-    if (kObserverRef) {
-        CFRunLoopRemoveObserver(CFRunLoopGetMain(), kObserverRef, kCFRunLoopCommonModes);
-        CFRelease(kObserverRef);
-        kObserverRef = nil;
+    //fade from green to red
+    double n = 1 - (fps/60);
+    double red = (255 * n);
+    double green = (255 * (1 - n)/2);
+    double blue = 0;
+    UIColor *color = [UIColor colorWithRed:red/255.0 green:green/255.0 blue:blue/255.0 alpha:1.0];
+    if (fps > 0.0) {
+        kTextLabel.backgroundColor = color;
+        kTextLabel.text = [NSString stringWithFormat:@"fps: %.2f", fps];
+    } else {
+        kTextLabel.backgroundColor = [UIColor lightGrayColor];
+        kTextLabel.text = nil;
     }
-    
-    if (kWatchdogTimer && dispatch_source_get_handle(kWatchdogTimer)) {
-        dispatch_source_cancel(kWatchdogTimer);
-    }
-
-    kWatchdogTimer = nil;
-}
-
-- (void)dealloc
-{
-    [[self class] stop];
 }
 
 + (void)setupStatusBarLabel
@@ -163,6 +125,13 @@ dispatch_source_t createDispatchTimer(uint64_t interval, uint64_t leeway, dispat
     label.frame = CGRectMake(0, 0, size.width, size.height);
     [TWYourStatusBar setCustomView:label];
     kTextLabel = label;
+}
+
+#pragma mark - Life Cycle
+
+- (void)dealloc
+{
+    [[self class] stop];
 }
 
 @end
