0f54b99c23af7b8405fff3a9b9647574f8e39af4
Christian Menschel <christian@tapwork.de>
2016-02-22 11:47:03 +0100

    Adds bars to visualise the frame rate

--- Classes/TWWatchdogInspectorStatusBarView.m
+++ Classes/TWWatchdogInspectorStatusBarView.m
@@ -8,10 +8,14 @@
 
 #import "TWWatchdogInspectorStatusBarView.h"
 
+static const double kBestFrameRate = 60.0;
+static const CGFloat kBarViewWidth = 10.0;
+static const CGFloat kBarViewPaddingX = 8.0;
+static const CGFloat kBarViewAnimationDuration = 2.0;
+
 @interface TWWatchdogInspectorStatusBarView ()
-
 @property (nonatomic, nonnull) UILabel *label;
-
+@property (nonatomic, nonnull) NSHashTable *barViews;
 @end
 
 @implementation TWWatchdogInspectorStatusBarView
@@ -27,6 +31,8 @@
         label.font = [UIFont systemFontOfSize:11];
         [self addSubview:label];
         _label = label;
+        
+        _barViews = [NSHashTable weakObjectsHashTable];
     }
     return self;
 }
@@ -46,22 +52,62 @@
 {
     _fps = fps;
     [self updateColorWithFPS:fps];
+    [self addBarWithFPS:fps];
 }
+
+#pragma mark - Private methods
 
 - (void)updateColorWithFPS:(double)fps
 {
     //fade from green to red
-    double n = 1 - (fps/60);
+    double n = 1 - (fps/kBestFrameRate);
     double red = (255 * n);
     double green = (255 * (1 - n)/2);
     double blue = 0;
     UIColor *color = [UIColor colorWithRed:red/255.0 green:green/255.0 blue:blue/255.0 alpha:1.0];
     if (fps > 0.0) {
-        self.backgroundColor = color;
         self.label.text = [NSString stringWithFormat:@"fps: %.2f", fps];
     } else {
-        self.backgroundColor = [UIColor lightGrayColor];
+        color = [UIColor lightGrayColor];
         self.label.text = nil;
+    }
+    
+    [UIView animateWithDuration:0.2 animations:^{
+        self.layer.backgroundColor = color.CGColor;
+    }];
+}
+
+- (void)addBarWithFPS:(double)fps
+{
+    CGFloat height = self.bounds.size.height * (fps / kBestFrameRate);
+    CGFloat xPos = self.bounds.size.width;
+    CGFloat yPos = self.bounds.size.height - height;
+    UIView *barView = [[UIView alloc] initWithFrame:CGRectMake(xPos, yPos, kBarViewWidth, height)];
+    barView.backgroundColor = [UIColor colorWithWhite:1.0 alpha:0.6];
+    [self addSubview:barView];
+    [self.barViews addObject:barView];
+    [self animateBarViews];
+}
+
+- (void)animateBarViews {
+    [self bringSubviewToFront:self.label];
+    for (UIView *barView in self.barViews) {
+        CGRect rect = barView.frame;
+        rect.origin.x = rect.origin.x - rect.size.width - kBarViewPaddingX;
+        [UIView animateWithDuration:kBarViewAnimationDuration animations:^{
+            barView.frame = rect;
+        } completion:^(BOOL finished) {
+            if (finished) {
+                [self removeBarViewIfNeeded:barView];
+            }
+        }];
+    }
+}
+
+- (void)removeBarViewIfNeeded:(UIView *)barView
+{
+    if (CGRectGetMaxX(barView.frame) <= 0) {
+        [barView removeFromSuperview];
     }
 }
 
