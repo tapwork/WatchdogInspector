193af4882af91874873ff2054825513d3cde34ab
Christian Menschel <christian@tapwork.de>
2016-03-09 20:42:12 +0100

    Removes swift example

--- SwiftExample/Pods/Target Support Files/WatchdogInspector/WatchdogInspector-umbrella.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "TWWatchdogInspector.h"
-#import "TWWatchdogInspectorStatusBarView.h"
-
-FOUNDATION_EXPORT double WatchdogInspectorVersionNumber;
-FOUNDATION_EXPORT const unsigned char WatchdogInspectorVersionString[];
-
--- SwiftExample/Pods/Target Support Files/YourStatusBar/YourStatusBar-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "TWYourStatusBar.h"
-
-FOUNDATION_EXPORT double YourStatusBarVersionNumber;
-FOUNDATION_EXPORT const unsigned char YourStatusBarVersionString[];
-
