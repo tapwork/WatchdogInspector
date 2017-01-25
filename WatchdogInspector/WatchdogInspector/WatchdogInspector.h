ed94e3ec6e2661666f91cbdeef538c0e08e9ad0f
Christian Menschel <christian@tapwork.de>
2017-01-25 10:24:38 +0100

    Adds carthage

---
+++ WatchdogInspector/WatchdogInspector/WatchdogInspector.h
@@ -0,0 +1,19 @@
+//
+//  WatchdogInspector.h
+//  WatchdogInspector
+//
+//  Created by Christian Menschel on 25.01.2017.
+//  Copyright © 2017 TAPWORK GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WatchdogInspector.
+FOUNDATION_EXPORT double WatchdogInspectorVersionNumber;
+
+//! Project version string for WatchdogInspector.
+FOUNDATION_EXPORT const unsigned char WatchdogInspectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WatchdogInspector/PublicHeader.h>
+
+
