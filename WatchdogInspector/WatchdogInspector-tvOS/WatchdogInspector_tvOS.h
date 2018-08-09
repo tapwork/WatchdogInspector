ede130f0f4c9021b89e65457f29c172675d5fdbb
Michele Dall'Agata <Michele@silvermoon.local>
2018-08-09 12:29:29 +0200

    port to tvOS

--- WatchdogInspector/WatchdogInspector-tvOS/WatchdogInspector_tvOS.h
+++ WatchdogInspector/WatchdogInspector-tvOS/WatchdogInspector_tvOS.h
@@ -17,3 +17,4 @@ FOUNDATION_EXPORT const unsigned char WatchdogInspector_tvOSVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <WatchdogInspector_tvOS/PublicHeader.h>
 
 
+#import "TWWatchdogInspector.h"
