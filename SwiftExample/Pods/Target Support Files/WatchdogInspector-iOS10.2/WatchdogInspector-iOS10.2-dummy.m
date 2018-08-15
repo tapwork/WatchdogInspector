98072271d721baeaf46a436cbd9828768d0e46f3
Christian Menschel <christian@tapwork.de>
2018-08-15 14:40:49 +0200

    Bump version 1.3.0

---
+++ SwiftExample/Pods/Target Support Files/WatchdogInspector-iOS10.2/WatchdogInspector-iOS10.2-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WatchdogInspector_iOS10_2 : NSObject
+@end
+@implementation PodsDummy_WatchdogInspector_iOS10_2
+@end
---
+++ SwiftExample/Pods/Target Support Files/WatchdogInspector-tvOS10.2/WatchdogInspector-tvOS10.2-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WatchdogInspector_tvOS10_2 : NSObject
+@end
+@implementation PodsDummy_WatchdogInspector_tvOS10_2
+@end
